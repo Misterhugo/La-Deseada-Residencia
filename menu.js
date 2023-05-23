af764ab124c51ff4ab57d486a3266eb0671558a8
kevinjoaquincarmona <66095625+kevinjoaquincarmona@users.noreply.github.com>
2023-05-23 04:03:25 -0300

    Add files via upload

--- menu.js
+++ menu.js
@@ -87,36 +87,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
-const apiKey = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'; // Reemplaza con tu propia API key de OpenWeatherMap
-
-// Función para obtener el clima actual
-function obtenerClima(ciudad) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      mostrarClima(data);
-    })
-    .catch(error => {
-      console.error('Error al obtener el clima:', error);
+  $(document).ready(function() {
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: 'https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=Washington%2CDC%2CUSA&contentType=csv&unitGroup=us&shortColumnNames=0',
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+        'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+      }
+    };
+  
+    $.ajax(settings).done(function(response) {
+      mostrarClima(response);
     });
-}
-
-// Función para mostrar los datos del clima en el HTML
-function mostrarClima(data) {
-  const ciudadElement = document.getElementById('ciudad');
-  const descripcionElement = document.getElementById('descripcion');
-  const temperaturaElement = document.getElementById('temperatura');
-  const humedadElement = document.getElementById('humedad');
-
-  ciudadElement.textContent = data.name;
-  descripcionElement.textContent = data.weather[0].description;
-  temperaturaElement.textContent = `Temperatura: ${Math.round(data.main.temp - 273.15)}°C`;
-  humedadElement.textContent = `Humedad: ${data.main.humidity}%`;
-}
-
-// Ejemplo de uso
-obtenerClima('Buenos Aires'); // Reemplaza 'Ciudad' con el nombre de la ciudad que deseas obtener el clima
+  
+    function mostrarClima(data) {
+      const ciudadElement = document.getElementById('ciudad');
+      const descripcionElement = document.getElementById('descripcion');
+      const temperaturaElement = document.getElementById('temperatura');
+      const humedadElement = document.getElementById('humedad');
+  
+      // Aquí puedes procesar y mostrar los datos del clima
+      console.log(data);
+  
+      // Actualiza los elementos HTML con los datos del clima
+      ciudadElement.textContent = 'Washington, DC, USA'; // Reemplaza con la ciudad correspondiente
+      descripcionElement.textContent = 'Descripción del clima'; // Reemplaza con la descripción del clima
+      temperaturaElement.textContent = 'Temperatura: 25°C'; // Reemplaza con la temperatura actual
+      humedadElement.textContent = 'Humedad: 80%'; // Reemplaza con el valor de humedad
+    }
+  });
+  
